c138d2c4d25f916c0fbfc9767fdbae11ca296ce2
Zhao Fu <zhaofu@google.com>
2026-07-06 13:19:09 -0700

    Remove two_sum.py example

--- two_sum.py
+++
@@ -1,7 +0,0 @@
-def two_sum(nums, target):
-    # O(N^2) time complexity
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
-    return []
