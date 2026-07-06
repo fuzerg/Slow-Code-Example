318583bd88a3a03143a346e5a5c78d3b0a2a24bc
fuzerg <fuzerg1@gmail.com>
2026-07-06 14:02:10 -0700

    Optimize data aggregation by replacing O(N*M) nested loop with O(N+M) hash map approach

--- data_aggregation.py
+++ data_aggregation.py
@@ -2,16 +2,16 @@ def aggregate_user_spending(users, transactions):
     # 'users' is a list of dicts: [{'id': 1, 'name': 'Alice'}, ...]
     # 'transactions' is a list of dicts: [{'user_id': 1, 'amount': 50.0}, ...]
     
+    # Pre-aggregate transaction totals by user_id to avoid nested O(N*M) loop
+    txn_totals = {}
+    for txn in transactions:
+        u_id = txn['user_id']
+        txn_totals[u_id] = txn_totals.get(u_id, 0.0) + txn['amount']
+    
     results = []
     for user in users:
-        total_spent = 0
-        # Inefficient nested loop scan
-        for txn in transactions:
-            if txn['user_id'] == user['id']:
-                total_spent += txn['amount']
-        
         results.append({
             'name': user['name'],
-            'total_spent': total_spent
+            'total_spent': txn_totals.get(user['id'], 0.0)
         })
     return results
